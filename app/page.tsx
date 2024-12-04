695821df1714b5b783ef7326022a696f5ed8c3d0
Gustavo Riedo <gh03012@gmail.com>
2024-12-04 07:20:48 -0300

    partial: adding index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,95 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+       <main className="w-screen">
+        <section className="w-full px-20 pt-20 pb-8">
+          <h1 className="font-questrial text-9xl">BPO em Marketing</h1>
+          <h2 className="w-3/5 font-questrial text-5xl">Se o seu marketing não vende, não é marketing, é Hobbie!</h2>
+        </section>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <section className="w-full">
+          <div className="flex gap-5 flex-wrap">
+            <div className="bg-impulse_green rounded-tr-3xl border py-24 px-14 flex flex-col gap-5">
+              <h2 className="font-questrial text-4xl w-fit">Branding</h2>
+              <p className="font-questrial text-base w-2/3">Não se trata de criar uma marca. Se trata de construir memórias e conectar corações. E nós fazemos isso por você</p>
+              <Link href={""} className="px-10 py-2 border border-black rounded-t-3xl rounded-br-3xl w-fit hover:bg-black hover:text-impulse_green transition">fale com a gente</Link>
+            </div>
+            <div className="py-14 px-32 h-fit rounded-3xl bg-gray-200 mt-auto">
+              <div className="w-16 h-32 relative animate-bounce">
+                <Image src="/down_arrow.svg" alt="Seta indicando para baixo" fill/>
+              </div>
+            </div>
+            <div className="bg-black rounded-tl-3xl border py-24 px-14 flex flex-col gap-5">
+              <h2 className="font-questrial text-4xl text-white w -fit">Gestão de Tráfego</h2>
+              <p className="font-questrial text-base text-white w-2/3">Transformamos a complexidade digital em solução para seu negócio</p>
+              <Link href={""} className="px-10 py-2 border text-white border-white rounded-t-3xl rounded-br-3xl w-fit hover:bg-white hover:text-black transition">alavanque sua marca!</Link>
+            </div>
+            <div className="w-10 h-10 relative">
+              <Image src={"/empresarios_passando.jpg"} alt="Imagem ilustrativa de negócios" fill/>
+            </div>
+            <div className="bg-black rounded-3xl border py-24 px-14 flex flex-col gap-5 text-impulse_green">
+              <h2>Social Media</h2>
+              <p>Cativamos e conectamos pessoas através de conteúdos realmente relevantes.</p>
+              <Link href={""} className="px-10 py-2 border border-impulse_green rounded-t-3xl rounded-br-3xl w-fit hover:bg-impulse_green hover:text-black transition">confira nossa solução</Link>
+            </div>
+            <div className="py-10 px-24 h-fit rounded-3xl bg-gray-200">
+              <div className="w-40 h-40 relative fill-impulse_purple">
+                <Image src="/star.svg" alt="" fill/>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* <section>
+          <h1>Nossa história</h1>
+          <div>
+            <h2>Somos especialistas em moldar o futuro de negócios.</h2>
+            <p>Em 2020, com a chegada da pandemia ao Brasil, tornou-se evidente que as marcas que não se adaptassem e se reposicionassem no mercado enfrentariam severas consequências dessa crise sanitária global. Antecipando esta realidade, agimos proativamente. Nossas estratégias permitiram impulsionar significativamente negócios que, contra todas as expectativas, não só se mantiveram firmes como também prosperaram em um cenário extremamente desafiador. Hoje, munidos de uma vasta expertise acumulada, especializamo-nos em encurtar o caminho para o sucesso de inúmeras empresas por todo o Brasil. Através de nossa metodologia inovadora de BPO em Marketing, nosso foco é capacitar as empresas a se concentrarem em suas competências centrais, enquanto nossos especialistas dedicam-se aos aspectos cruciais do marketing. Este modelo tem se mostrado eficaz, resultando em maior eficiência operacional, redução significativa de custos, acesso a tecnologias de ponta e expertise especializada, além da flexibilidade para escalar as operações de marketing de acordo com as necessidades dinâmicas do mercado.</p>
+            <Link href={""}>Saiba mais</Link>
+          </div>
+        </section>
+
+        <section>
+          <h1>Serviços</h1>
+          <div>
+            <div>
+              <div className="w-10 h-10 relative">
+                <Image src="/logo_arrow.svg" alt="Logo Impulse BPO" fill/>
+              </div>
+              <h2>Estratégia de comunicação</h2>
+              <p>Implementamos uma estratégia de comunicação que é um plano coordenado para entregar uma mensagem consistente e eficaz ao seu público-alvo, utilizando diferentes canais e ferramentas de mídia.</p>
+            </div>
+            <div>
+              <div className="w-10 h-10 relative">
+                <Image src="/logo_arrow.svg" alt="Logo Impulse BPO" fill/>
+              </div>
+              <h2>Transmitir a mensagem da marca</h2>
+              <p>Nós transmitimos a mensagem da sua marca ao comunicar de forma clara e eficaz os valores, missão e características únicas da sua empresa, utilizando diversos canais e métodos de comunicação.</p>
+            </div>
+            <div>
+              <div className="w-10 h-10 relative">
+                <Image src="/logo_arrow.svg" alt="Logo Impulse BPO" fill/>
+              </div>
+              <h2>Estratégia de Tráfego Pago</h2>
+              <p>Nós desenvolvemos uma estratégia de tráfego pago ao planejar e executar campanhas de publicidade online pagas, visando aumentar a visibilidade e atrair mais visitantes e leads qualificados para seu site ou plataforma digital.</p>
+            </div>
+            <div>
+              <div className="w-10 h-10 relative">
+                <Image src="/logo_arrow.svg" alt="Logo Impulse BPO" fill/>
+              </div>
+              <h2>Identidade de marca</h2>
+              <p>Criaremos a identidade da  marca, que  é o conjunto único de elementos visuais e verbais que usamos para diferenciar e comunicar nossa mensagem, incluindo logo, cores, tipografia e tom de voz.</p>
+            </div>
+          </div>
+        </section> */}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer>
+
       </footer>
-    </div>
+    </>
   );
 }
